1972084279c414b00cb72b602813641905270e90
unknown <slobxns@gmail.com>
2017-11-22 20:31:25 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+site_in_progress
+---
---
+++ js/site_in_progress.js
@@ -0,0 +1,61 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if(t.seconds <= 30 && t.seconds % 2 == 0) {
+      document.getElementById("clockdiv").style.color = "#F00";
+    } else{
+      document.getElementById("clockdiv").style.color = "#FFF";
+    }
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 1 * 1 * 1 * 33 * 1000);
+initializeClock('clockdiv', deadline);
+
+function openFB(){
+window.open("http://www.facebook.com");
+}
+
+function openLinkedin(){
+window.open("http://www.linkedin.com");
+}
+
+function openTwitter(){
+window.open("http://www.twitter.com");
+}
+
+
